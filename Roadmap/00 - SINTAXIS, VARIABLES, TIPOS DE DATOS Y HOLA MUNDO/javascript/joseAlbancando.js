26cbbeb88eecb98ecc19c68a2da3f2da7041221f
essunombre <alb21001@byui.edu>
2024-07-23 22:03:33 -0500

    adding mine

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/joseAlbancando.js
@@ -0,0 +1,48 @@
+// https://www.javascript.com/
+// Javascript
+
+// This is one line comment
+
+/*
+This is a 
+multiple line Comment
+*/
+
+console.log("hola");
+let myVariable = "Jose";
+console.log(myVariable)
+myVariable = "David";
+console.log(myVariable)
+
+// This onw will not change
+const myVariable2 = "constante";
+console.log(myVariable2)
+
+// Const cannot change.
+// myVariable2 = "constantes";
+// console.log(myVariable2)
+
+// string
+let cadenTexto = "I am a string";
+// int
+let age = 27;
+// float
+let temperature = 20.5
+// booleans
+let verdadero = true;
+let falso = false;
+
+console.log(typeof(myVariable))
+console.log(typeof(myVariable2))
+console.log(typeof(cadenTexto))
+console.log(typeof(age))
+console.log(typeof(temperature))
+console.log(typeof(verdadero))
+console.log(typeof(falso))
+console.log(typeof(null))
+
+
+
+
+
+
